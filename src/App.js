5a1cb7c1efbf48e1b75abcc3663dc72673c821bf
Katarina Avendano <avendanokatarina@gmail.com>
2020-08-31 22:10:04 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+        <h1>Weather App</h1>
       <footer>
         This project was coded by Katarina Avendaño and is
       <a href="https://github.com/Katarina175/react-weather-app-project" target="blank"> open sourced on GitHub</a></footer>
+      </div>
     </div>
   );
 }
