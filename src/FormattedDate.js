1903ee49239fe26adf83b38c582325516c710f4d
Katarina Avendano <avendanokatarina@gmail.com>
2020-09-04 16:54:22 -0400

    Updated date in weather component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function FormattedDate (props) {
     // console.log (props.date);
-    let day = props.date.getDay();
+    let date = props.date.getDate();
     let year = props.date.getFullYear();
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October","November", "December"];
     let month = months[props.date.getMonth()];
     return <div>
-        {month} {day}, {year}
+        {month} {date}, {year}
     </div>;
 }
